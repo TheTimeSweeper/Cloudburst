e8a038a563a854f3937c79dd8fd190fcc3bdac01
Someone-Else-Was-Taken <justsomeoneelse92@gmail.com>
2022-12-01 03:42:36 -0800

    Added Assetbundle

--- Cloudburst/Cloudburst.cs
+++ Cloudburst/Cloudburst.cs
@@ -3,6 +3,7 @@ using Cloudburst.Items;
 using R2API;
 using R2API.Utils;
 using RoR2;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
@@ -18,10 +19,13 @@ namespace Cloudburst
         public const string PluginName = "Cloudburst";
         public const string PluginVersion = "0.0.0";
 
+        public static AssetBundle CloudburstAssets;
+
         public void Awake()
         {
             Log.Init(Logger);
 
+            GetBundle();
             SetupItems();
            
 
@@ -34,6 +38,33 @@ namespace Cloudburst
             FabinhoruDagger.Setup();
             GlassHarvester.Setup();
             JapesCloak.Setup();
+            RiftBubble.Setup();
+        }
+
+        //Stripped from SomewhereElse
+        public void GetBundle()
+        {
+            if (CloudburstAssets == null)
+            {
+                try
+                {
+                    using (var assetStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Cloudburst.Assets.cloudburst"))
+                    {
+                        CloudburstAssets = AssetBundle.LoadFromStream(assetStream);
+                    }
+                }
+                catch
+                {
+                    
+                }
+            }
+            if (CloudburstAssets != null)
+            {
+                Log.Info("Successfully loaded Asset Bundle");
+            } else
+            {
+                Log.Error("AAAAAA");
+            }
         }
     }
 }
