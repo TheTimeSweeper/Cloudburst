9f3b276491e159f47b39fd03d24662a6b9033ff8
Someone-Else-Was-Taken <justsomeoneelse92@gmail.com>
2022-12-01 03:43:07 -0800

    Added Rift Indicator

---
+++ Cloudburst/Items/RiftBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RoR2;
+using UnityEngine;
+
+namespace Cloudburst.Items
+{
+    internal class RiftBehaviour : CharacterBody.ItemBehavior
+    {
+        public GameObject indicatorInstance;
+        public BuffWard ward;
+        public void OnEnable()
+        {
+            indicatorInstance = GameObject.Instantiate<GameObject>(RiftBubble.riftBubbleIndicator, transform);
+            if(indicatorInstance != null)
+            {
+                ward = indicatorInstance.GetComponent<BuffWard>();
+                ward.teamFilter.teamIndex = TeamComponent.GetObjectTeam(gameObject);
+            }
+        }
+
+        public void FixedUpdate()
+        {
+            if(ward != null)
+            {
+                ward.radius = stack * 5;
+            }
+        }
+
+        public void OnDisable()
+        {
+            if(indicatorInstance != null)
+            {
+                GameObject.Destroy(indicatorInstance);
+            }
+        }
+    }
+}
